d0a32c91515f51941f6d68303a915d5df83f8148
Tim Evens <tievens@cisco.com>
2024-10-22 12:35:21 -0700

    Update doc comments

--- src/state.h
+++ src/state.h
@@ -15,7 +15,7 @@ namespace laps {
          * Map of subscribes (e.g., track alias) sent to announcements
          *
          * @example
-         *      track_alias_set = announce_active[track_namespace_hash][connection_handle]
+         *      track_alias_set = announce_active[track_namespace_hash, connection_handle]
          */
         std::map<std::pair<quicr::TrackNamespaceHash, quicr::ConnectionHandle>, std::set<quicr::messages::TrackAlias>>
           announce_active;
@@ -24,7 +24,7 @@ namespace laps {
          * Active publisher/announce subscribes that this relay has made to receive objects from publisher.
          *
          * @example
-         *      track_delegate = pub_subscribes[track_alias][conn_id]
+         *      track_delegate = pub_subscribes[track_alias, connection handle]
          */
         std::map<std::pair<quicr::messages::TrackAlias, quicr::ConnectionHandle>,
                  std::shared_ptr<quicr::SubscribeTrackHandler>>
@@ -44,7 +44,7 @@ namespace laps {
          * @note This indexing intentionally prohibits per connection having more
          *           than one subscribe to a full track name.
          *
-         * @example track_handler = subscribes[track_alias][connection_handle]
+         * @example track_handler = subscribes[track_alias, connection_handle]
          */
         std::map<std::pair<quicr::messages::TrackAlias, quicr::ConnectionHandle>, SubscribePublishHandlerInfo>
           subscribes;
@@ -54,14 +54,14 @@ namespace laps {
          *      Used to lookup the track alias for a given subscribe ID
          *
          * @example
-         *      track_alias = subscribe_alias_sub_id[conn_id][subscribe_id]
+         *      track_alias = subscribe_alias_sub_id[connection handle, subscribe_id]
          */
         std::map<std::pair<quicr::ConnectionHandle, quicr::messages::SubscribeId>, quicr::messages::TrackAlias>
           subscribe_alias_sub_id;
 
         /**
          * Map of subscribes set by namespace and track name hash
-         *      Set<subscribe_who> = subscribe_active[track_namespace_hash][track_name_hash]
+         *      Set<subscribe_who> = subscribe_active[track_namespace_hash, track_name_hash]
          */
         struct SubscribeWho
         {
