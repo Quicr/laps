404703262dd9973007e2a45f320255a94a5ffc42
Rich Logan <rich@richlogan.co.uk>
2025-01-24 10:28:23 +0000

    Add isCached extension

--- src/client_manager.cc
+++ src/client_manager.cc
@@ -583,9 +583,12 @@ namespace laps {
                     SPDLOG_DEBUG("Subscribe: Publishing object from cache: Group {0} is Object {1}",
                                 object.headers.group_id,
                                 object.headers.object_id);
-                    pub_track_h->PublishObject(object.headers, object.data);
-                    // TODO: Check the reason for sleep. If not, we are missing few objects
-                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                    auto headers = object.headers;
+                    auto extensions = headers.extensions.value_or(quicr::Extensions());
+                    const std::uint64_t cached = 100;
+                    extensions[cached] = {0x01};
+                    headers.extensions = extensions;
+                    pub_track_h->PublishObject(headers, object.data);
                 }
             });
 
